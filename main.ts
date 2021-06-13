bca1d975778892ee886af1561bf457aa6a0de793
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-13 15:59:56 +0100

    light up centre led when button A held down

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
-let colorbit_51bit = colorbit.initColorBit(DigitalPin.P1, BitColorMode.RGB)
+input.onButtonPressed(Button.A, function () {
+	
+})
+function BullsEye () {
+    colorbit_51bit.drawColorBit(2, 2, colorbit.colors(BitColors.Green))
+    basic.pause(100)
+    colorbit_51bit.clear()
+}
+let colorbit_51bit: colorbit.Strip = null
+colorbit_51bit = colorbit.initColorBit(DigitalPin.P1, BitColorMode.RGB)
 colorbit_51bit.clear()
 colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Red))
 basic.forever(function () {
-	
+    while (input.buttonIsPressed(Button.A)) {
+        BullsEye()
+    }
 })
