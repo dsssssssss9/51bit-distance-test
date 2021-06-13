8d90252f9351e96b20a2d3a3104ca27eb471b8c8
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-13 15:55:20 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+let colorbit_51bit = colorbit.initColorBit(DigitalPin.P1, BitColorMode.RGB)
+colorbit_51bit.clear()
+colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Red))
+basic.forever(function () {
+	
+})
