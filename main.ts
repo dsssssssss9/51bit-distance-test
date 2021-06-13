f9c5cbf7323c48db09b51c454f11b0f589d2d746
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-13 16:21:16 +0100

    define function for outer ring

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
+function Outer () {
+    colorbit_51bit.clear()
+    for (let Count1 = 0; Count1 <= 4; Count1++) {
+        colorbit_51bit.drawColorBit(0, Count1, colorbit.colors(BitColors.Red))
+        colorbit_51bit.drawColorBit(Count1, 0, colorbit.colors(BitColors.Red))
+        colorbit_51bit.drawColorBit(Count1, 4, colorbit.colors(BitColors.Red))
+        colorbit_51bit.drawColorBit(4, Count1, colorbit.colors(BitColors.Red))
+    }
+}
 input.onButtonPressed(Button.A, function () {
-	
+    Outer()
 })
 function BullsEye () {
     colorbit_51bit.drawColorBit(2, 2, colorbit.colors(BitColors.Green))
@@ -11,7 +20,5 @@ colorbit_51bit = colorbit.initColorBit(DigitalPin.P1, BitColorMode.RGB)
 colorbit_51bit.clear()
 colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Red))
 basic.forever(function () {
-    while (input.buttonIsPressed(Button.A)) {
-        BullsEye()
-    }
+	
 })
