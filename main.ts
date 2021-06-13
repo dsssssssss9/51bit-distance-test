d459de1c9e10db8b13aacde983985460505f05ba
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-13 17:02:48 +0100

    add sonar extension for sr04 sensor

--- main.ts
+++ main.ts
@@ -32,6 +32,6 @@ function BullBed () {
 }
 let colorbit_51bit: colorbit.Strip = null
 colorbit_51bit = colorbit.initColorBit(DigitalPin.P1, BitColorMode.RGB)
-colorbit_51bit.setBrightness(64)
+colorbit_51bit.setBrightness(32)
 colorbit_51bit.clear()
 colorbit_51bit.showColorIcon(ColorIcon.ChristmasTree, colorbit.colors(BitColors.Red))
